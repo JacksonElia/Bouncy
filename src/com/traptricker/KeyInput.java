e1c6f4c7de6c42d98c529bf03881f8fbf37dd228
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-02-19 13:31:08 -0500

    You can press escape to close the program

--- src/com/traptricker/KeyInput.java
+++ src/com/traptricker/KeyInput.java
@@ -20,16 +20,8 @@ public class KeyInput extends KeyAdapter {
         super.keyPressed(e);
         // This int corresponds to the key pressed
         int key = e.getKeyCode();
-
-        // Example for how to use input to move the player
-        for (GameObject object : handler.objects) {
-            if (object.getId() == ID.Player) {
-                if (key == KeyEvent.VK_W) object.setYVelocity(-5);
-                if (key == KeyEvent.VK_S) object.setYVelocity(5);
-                if (key == KeyEvent.VK_A) object.setXVelocity(-5);
-                if (key == KeyEvent.VK_D) object.setXVelocity(5);
-            }
-        }
+        // Closes program when escape is pressed
+        if (key == KeyEvent.VK_ESCAPE) System.exit(1);
     }
 
     // Called whenever a key is released
@@ -37,14 +29,5 @@ public class KeyInput extends KeyAdapter {
     public void keyReleased(KeyEvent e) {
         super.keyReleased(e);
         int key = e.getKeyCode();
-        // Moves the player with wasd
-        for (GameObject object : handler.objects) {
-            if (object.getId() == ID.Player) {
-                if (key == KeyEvent.VK_W && object.getYVelocity() == -5) object.setYVelocity(0);
-                if (key == KeyEvent.VK_S && object.getYVelocity() == 5) object.setYVelocity(0);
-                if (key == KeyEvent.VK_A && object.getXVelocity() == -5) object.setXVelocity(0);
-                if (key == KeyEvent.VK_D && object.getXVelocity() == 5) object.setXVelocity(0);
-            }
-        }
     }
 }

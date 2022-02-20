f932713f13242d3c4a697e18d68e9298eb6f544f
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-02-20 11:29:26 -0500

    Added score and level systems to the HUD

--- src/com/traptricker/HUD.java
+++ src/com/traptricker/HUD.java
@@ -5,9 +5,12 @@ import java.awt.*;
 public class HUD {
 
     public static int health = 100;
+    public static int score = 0;
+    public static int level = 1;
 
     public void tick() {
-
+        score++;
+        level = Math.floorDiv(score, 1000);
     }
 
     public void render(Graphics g) {
@@ -15,6 +18,9 @@ public class HUD {
         g.fillRect(20, 20, 300, 40);
         g.setColor(Color.red);
         g.fillRect(24, 24, 292 * health / 100, 32);
+        g.setColor(Color.white);
+        g.drawString(String.format("Score: %d", score), 20, 80);
+        g.drawString(String.format("Level: %d", level), 20, 95);
     }
 
 }
